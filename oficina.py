aed0ade428a831b731974160dc4fe4b67e5f0b91
0001134239-dotcom <0001134239@senaimgaluno.com.br>
2026-03-23 16:03:13 -0300

    Update oficina.py

--- oficina.py
+++ oficina.py
@@ -199,8 +199,7 @@ with tab1:
         item = resultado['item'].values[0]
         armario = resultado['armario'].values[0]
         prateleira = resultado['prateleira'].values[0]
-        st.markdown(f'Armario:'+f'{A}')
-        A = st.markdown({item})
+        st.markdown(f'Armario: {item}')
         
     st.divider()
 
